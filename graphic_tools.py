1491e823f864f1ba0270fcd65fbe1a05dafba641
Javier Fumanal <javierfumanalidocin@gmail.com>
2018-03-20 20:41:07 +0100

    Plotly comenzado

--- graphic_tools.py
+++ graphic_tools.py
@@ -6,6 +6,9 @@ Created on Tue Jan 16 18:22:03 2018
 """
 
 import pandas
+import plotly.plotly as py
+import plotly.graph_objs as go
+import numpy as np
 
 from ggplot import ggplot, geom_line, aes, xlab,ylab, ggtitle
 from matplotlib import colors as mcolors
@@ -41,4 +44,102 @@ def plot_line(X,y,title=None,labelx=None,labely=None,save=False, colors=None):
     else:   
         return p
 
+def plotly_line_plot(df, columna):
+    '''
+    
+    A simple plotly plot thay creates a line plot with a variable from a data frame
+    '''
+
+    data = [
+    go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df[columna]
+    )
+    ]   
+    return py.plot(data, filename=df['ticker'][0].replace('/', "_")+ columna)
+
+def full_plot(df, segmentos):
+    '''
+    
+    A simple plotly plot thay creates a line plot with a variable from a data frame
+    '''
+    lineas_divisorias = []
+    escala = max(df.drop('volumen',axis=1).select_dtypes(include=[np.number]).max())
+    for i in segmentos:
+        coordenada = i[1]
+       # Line Vertical
+        linea = {
+            'type': 'line',
+            'x0': df.index[coordenada],
+            'y0': 0,
+            'x1': df.index[coordenada],
+            'y1': escala,
+            'line': {
+                'color': 'rgb(255, 0, 0)',
+                'width': 3,
+                'dash':'dashdot'
+            },
+        }
+        lineas_divisorias.append(linea)
+        
+    cierre = go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df['cierre']
+    )
+    apertura = go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df['apertura']
+    )
+    
+    minimo = go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df['minimo']
+    )
+    
+    maximo = go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df['maximo']
+    )
+    
+    volumen = go.Scatter(
+        x=df['fecha'], # assign x as the dataframe column 'x'
+        y=df['volumen']/np.max(df['volumen']) * escala
+    )
+    vtitle = 'Volumen (Factor de escala: ' + str(escala / np.max(df['volumen']))+ ')'
+    data = [cierre, apertura, minimo, maximo, volumen]
+    updatemenus = list([
+    dict(type="buttons",
+         active=-1,
+         buttons=list([   
+            dict(label = 'Cierre',
+                 method = 'update',
+                 args = [{'visible': [True, False, False, False, False]},
+                         {'title': 'Cierre'}]),
+            dict(label = 'Apertura',
+                 method = 'update',
+                 args = [{'visible': [False, True, False, False, False]},
+                         {'title': 'Apertura'}]),
+            dict(label = 'Mínimo',
+                 method = 'update',
+                 args = [{'visible': [False, False, True, False, False]},
+                         {'title': 'Minimo'}]),
+            dict(label = 'Máximo',
+                 method = 'update',
+                 args = [{'visible': [False, False, False, True, False]},
+                         {'title': 'Maximo'}]),
+            dict(label = 'Volumen',
+                 method = 'update',
+                 args = [{'visible': [False, False, False, False, True]},
+                         {'title': vtitle}])
+        ]),
+    )
+    ])
+    layout = dict(title='Financial records', showlegend=False,
+              updatemenus=updatemenus,
+              shapes=lineas_divisorias)
+    fig = dict(data=data, layout=layout)
+    
+    return py.plot(fig, filename=df['ticker'][0].replace('/', "_"))
+
+    
     
